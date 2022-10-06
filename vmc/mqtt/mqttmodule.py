cbe6f1addb65da9fb32e1f4c9eb5ae426a858efa
literallyanything <hunterbaker@me.com>
2022-10-06 12:50:31 -0400

    Fix mqtt so it doesnt print an error

--- vmc/mqtt/mqttmodule.py
+++ vmc/mqtt/mqttmodule.py
@@ -2,7 +2,7 @@ from bell.avr.mqtt.client import MQTTModule as BadMQTTModule
 
 
 class MQTTModule(BadMQTTModule):
+    @DeprecationWarning
     def __init__(self):
-        super().__init__()
         self.mqtt_host = "localhost"
         self.mqtt_port = 1883
--- vmc/mqtt_client.py
+++ vmc/mqtt_client.py
@@ -29,7 +29,6 @@ class MQTTClient:
         self.client.on_connect = self._on_connect
         self.client.on_disconnect = self._on_disconnect
         self.client.on_message = self._on_message
-        self.client.loop_start()
 
         self.topic_map = {}
         self._connected = False
