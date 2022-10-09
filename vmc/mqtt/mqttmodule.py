e5456aba6b1a54dde0efa622ad15f3ed25d13b96
literallyanything <hunterbaker@me.com>
2022-10-08 22:35:59 -0400

    Add warning to mqttmodule.py

--- vmc/mqtt/mqttmodule.py
+++ vmc/mqtt/mqttmodule.py
@@ -1,8 +1,10 @@
 from bell.avr.mqtt.client import MQTTModule as BadMQTTModule
+from loguru import logger
 
 
 class MQTTModule(BadMQTTModule):
     @DeprecationWarning
     def __init__(self):
+        logger.warning("MQTTModule is deprecated, use MQTTClient instead")
         self.mqtt_host = "localhost"
         self.mqtt_port = 1883
