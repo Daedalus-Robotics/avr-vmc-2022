a5f88de0d0f81bd173bd4ba698e5e196731d9280
literallyanything <hunterbaker@me.com>
2022-10-09 15:55:49 -0400

    Reformat

--- vmc/utils.py
+++ vmc/utils.py
@@ -1,6 +1,9 @@
-def map(x, in_min, in_max, out_min, out_max):
+def map(x: float | int,
+        in_min: float | int, in_max: float | int,
+        out_min: float | int, out_max: float | int
+        ) -> float | int:
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 
 
-def constrain(val, min_val, max_val):
+def constrain(val: float | int, min_val: float | int, max_val: float | int) -> float | int:
     return min(max_val, max(min_val, val))
