1fc9b836bd189af00ddd04adbea7e9c078ab7b11
literallyanything <hunterbaker@me.com>
2022-10-10 15:11:29 -0400

    Add more parameters for qos and properties

--- vmc/mqtt_client.py
+++ vmc/mqtt_client.py
@@ -1,9 +1,12 @@
 import json
 import time
 from threading import Thread
+from typing import Any, Callable
 
 from loguru import logger
 from paho.mqtt import client as mqtt
+
+DEFAULT_QOS = 1
 
 
 class MQTTClient:
@@ -27,7 +30,7 @@ class MQTTClient:
         self.client.on_disconnect = self._on_disconnect
         self.client.on_message = self._on_message
 
-        self.topic_map = {}
+        self.topic_map: dict[str, tuple[Callable, bool, int, Any, Any]] = {}
         self._connected = False
 
     @property
@@ -47,15 +50,16 @@ class MQTTClient:
         if self.is_connected:
             self.client.disconnect(mqtt.DISCONNECT)
 
-    def _on_connect(self, _: mqtt.Client, __: any, ___: dict, rc: int):
+    def _on_connect(self, _: mqtt.Client, __: Any, ___: dict, rc: int):
         self._connected = True
         logger.debug(f"Connected with result {rc}")
 
-        for topic in self.topic_map.keys():
-            self.client.subscribe(topic)
+        for topic, info in self.topic_map.items():
+            callback, _, qos, options, properties = info
+            self.client.subscribe(topic, qos = qos, options = options, properties = properties)
             logger.success(f"Subscribed to: {topic}")
 
-    def _on_disconnect(self, _: mqtt.Client, __: any, ___: dict, rc: int):
+    def _on_disconnect(self, _: mqtt.Client, __: Any, ___: dict, rc: int):
         logger.debug(f"Disconnected with result {rc}")
         if self.retry and rc is not mqtt.DISCONNECT:
             Thread(target = self._reconnect_loop).start()
@@ -65,31 +69,40 @@ class MQTTClient:
             self.connect()
             time.sleep(2)
 
-    def _on_message(self, _: mqtt.Client, __: any, msg: mqtt.MQTTMessage):
+    def _on_message(self, _: mqtt.Client, __: Any, msg: mqtt.MQTTMessage):
         if msg.topic in self.topic_map:
             try:
-                self.topic_map[msg.topic](json.loads(msg.payload))
+                topic_tuple = self.topic_map[msg.topic]
+                callback = topic_tuple[0]
+                payload: dict | bytes = msg.payload
+                if topic_tuple[1]:
+                    payload = json.loads(msg.payload)
+                callback(payload)
             except (AttributeError, TypeError, json.JSONDecodeError) as e:
                 logger.warning(f"Filed to run callback for topic {msg.topic}")
                 logger.warning(e)
 
-    def send_message(self, topic: str, payload: any) -> bool:
+    def send_message(
+            self, topic: str, payload: Any, qos: int = DEFAULT_QOS, retain: bool = False, properties: Any = None
+    ) -> bool:
         serializable, json_payload = is_serializable(payload)
         if serializable:
             payload = json_payload
         try:
-            self.client.publish(topic, payload)
+            self.client.publish(topic, payload, qos = qos, retain = retain, properties = properties)
             return True
         except TypeError:
             return False
 
-    def register_callback(self, topic: any, callback: callable) -> bool:
+    def register_callback(
+            self, topic: str, callback: Callable, is_json: bool = True, qos: int = DEFAULT_QOS, options: Any = None, properties: Any = None
+    ) -> bool:
         return_val = False
         if topic not in self.topic_map:
-            self.topic_map[topic] = callback
+            self.topic_map[topic] = (callback, is_json, qos, options, properties)
             return_val = True
             if self._connected:
-                self.client.subscribe(topic)
+                self.client.subscribe(topic, qos = qos, options = options, properties = properties)
                 logger.success(f"Subscribed to: {topic}")
         return return_val
 
@@ -98,7 +111,7 @@ class MQTTClient:
             self.register_callback(topic, callback)
 
 
-def is_serializable(x) -> (bool, str):
+def is_serializable(x: Any) -> (bool, str):
     try:
         json_string = json.dumps(x)
         return True, json_string
