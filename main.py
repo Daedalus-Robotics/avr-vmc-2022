83cec83fff5782993399f20566fab927bd9d0ea4
literallyanything <hunterbaker@me.com>
2022-10-11 12:44:11 -0400

    Add method to restart vmc

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import subprocess
+
 from systemctl import Service
 
 from vmc.mqtt_client import MQTTClient
@@ -8,6 +10,20 @@ mqtt_client = MQTTClient.get("localhost", 1883, True)
 status = Status()
 
 pcc: PeripheralControlComputer
+mavp2p: Service
+
+
+def restart_vmc() -> None:
+    pcc.end()
+    status.register_status("pcc", False)
+
+    mavp2p.stop()
+    status.register_status("mavp2p", False)
+
+    status.update_status("vmc", False)
+    mqtt_client.disconnect()
+    subprocess.Popen(["sleep", "8", ";sudo", "reboot"])
+
 
 if __name__ == '__main__':
     pcc = PeripheralControlComputer()
@@ -19,6 +35,8 @@ if __name__ == '__main__':
     mavp2p = Service("mavp2p.service")
     status.register_status("mavp2p", mavp2p.is_active, mavp2p.restart)
     mavp2p.on_state = lambda state: status.update_status("mavp2p", state)
+
+    status.register_status("vmc", True, restart_vmc)
 
     mqtt_client.connect()
     status.send_update()
