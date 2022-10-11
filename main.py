c6d91f2d4fa2df8db62482fa92582931bf5e9b88
literallyanything <hunterbaker@me.com>
2022-10-11 12:27:03 -0400

    Add status for mavp2p

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from systemctl import Service
+
 from vmc.mqtt_client import MQTTClient
 from vmc.pcc import PeripheralControlComputer
 from vmc.status import Status
@@ -13,6 +15,10 @@ if __name__ == '__main__':
     pcc.on_state = lambda state: status.update_status("pcc", state)
     pcc.begin()
     pcc.begin_mqtt()
+
+    mavp2p = Service("mavp2p.service")
+    status.register_status("mavp2p", mavp2p.is_active, mavp2p.restart)
+    mavp2p.on_state = lambda state: status.update_status("mavp2p", state)
 
     mqtt_client.connect()
     status.send_update()
