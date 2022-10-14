febb6db3b695b9098ce5cd80a26a712d9c6395c8
literallyanything <hunterbaker@me.com>
2022-10-13 21:28:25 -0400

    Deprecate the mqtt module correctly

--- vmc/mqtt/mqttmodule.py
+++ vmc/mqtt/mqttmodule.py
@@ -1,10 +1,11 @@
 from bell.avr.mqtt.client import MQTTModule as BadMQTTModule
+from deprecated.classic import deprecated
 from loguru import logger
 
 
 class MQTTModule(BadMQTTModule):
-    @DeprecationWarning
-    def __init__(self):
-        logger.warning("MQTTModule is deprecated, use MQTTClient instead")
+    @deprecated
+    def __init__(self) -> None:
+        super().__init__()
         self.mqtt_host = "localhost"
         self.mqtt_port = 1883
